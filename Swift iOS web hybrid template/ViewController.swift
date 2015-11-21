63a19dd6a6b6181bb6b47e52d972db4edebfd211
Willson Smith <me@willsonsmith.com>
2015-11-20 20:43:56 -0500

    can now call javascript from swift

--- Swift iOS web hybrid template/ViewController.swift
+++ Swift iOS web hybrid template/ViewController.swift
@@ -22,17 +22,42 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         let url = NSBundle.mainBundle().URLForResource("index", withExtension:"html");
-        //let request = NSURLRequest(URL: url!);
         self.webView!.loadFileURL(url!, allowingReadAccessToURL: url!);
 
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    func executeJavascript(functionToRun:String, argument:String?) {
+        var functionName:String;
+        var arg:String;
+        if ((argument) != nil) {
+            arg = argument!;
+        } else {
+            arg = "";
+        }
+        
+        functionName = "\(functionToRun)('\(arg)')";
+        print(functionName);
+        self.webView!.evaluateJavaScript(functionName, completionHandler: handleJavascriptCompletion)
+    }
+    
+    func currentVersion() -> String {
+        return "Swift iOS web hybrid template 1.0.0";
+    }
+    
+    func getCurrentVersion() {
+        executeJavascript("addVersion", argument:currentVersion())
+    }
+    
+    func handleJavascriptCompletion(object:AnyObject?, error:NSError?) -> Void {
+        if (error != nil) {
+            print(error);
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
